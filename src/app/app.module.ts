5b056c2b34e2d87bf48cb9737cdb3e1aa69b92d3
Maryam <maryam@ingenius.com>
2019-04-26 10:10:33 -0400

    Created new creature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatureComponent } from './creature/creature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatureComponent
   ],
   imports: [
     BrowserModule,
