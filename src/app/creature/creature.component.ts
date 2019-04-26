734e055551ecaf26e3d71ce982f4374f7f75deaa
Maryam <maryam@ingenius.com>
2019-04-26 14:55:06 -0400

    Generate the dude from ts

--- src/app/creature/creature.component.ts
+++ src/app/creature/creature.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { IGene } from '../gene';
 
 @Component({
-  selector: 'app-creature',
-  templateUrl: './creature.component.html',
-  styleUrls: ['./creature.component.sass']
+	selector: 'app-creature',
+	templateUrl: './creature.component.html',
+	styleUrls: ['./creature.component.scss']
 })
 export class CreatureComponent implements OnInit {
 
-  constructor() { }
+	protected gene = {
+		Color: "", 
+		Height:'Tall', 
+		Orientation: "Vertical",
 
-  ngOnInit() {
-  }
+		Head2: "icon-square", 
+		Body1: "icon-square", 
+		Body2: "icon-square",
+		Arm1: "icon-circle",  
+		Arm2: "icon-circle",  
+		Arm3: "icon-circle",  
+		Arm4: "icon-circle",
+		Leg1: "icon-more-colours"
+	}
+	constructor() { }
 
+	ngOnInit() {
+	}
 }
