e6d9215c04cf2a63099405d5ded6005305f30c4b
INGENIUS\justin.fleming <justin.fleming@ingenius.com>
2019-04-26 11:04:52 -0400

    added a skeleton gene class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatureComponent } from './creature/creature.component';
 import { GeneratorButtonComponent } from './generator-button/generator-button.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		GeneratorButtonComponent
+		GeneratorButtonComponent,
+		CreatureComponent
 	],
 	imports: [
 		BrowserModule,
--- src/app/gene.ts
+++ src/app/gene.ts
@@ -22,22 +22,66 @@ export type IHeight = 'Short' | 'Tall';
 
 export class Gene implements IGene
 {
-	protected readonly _orientation: IOrientation;
-	protected readonly _height: IHeight;
-	protected readonly _color: string;
-	protected readonly _head1: string;
-	protected readonly _head2: string;
-	protected readonly _head3: string;
-	protected readonly _torso1: string;
-	protected readonly _torso2: string;
-	protected readonly _appendage1: string;
-	protected readonly _appendage2: string;
-	protected readonly _appendage3: string;
-	protected readonly _appendage4: string;
-	protected readonly _appendage5: string;
-	protected readonly _appendage6: string;
-	protected readonly _appendage7: string;
+	protected _orientation: IOrientation;
+	protected _height: IHeight;
+	protected _color: string;
+	protected _head1: string;
+	protected _head2: string;
+	protected _head3: string;
+	protected _torso1: string;
+	protected _torso2: string;
+	protected _appendage1: string;
+	protected _appendage2: string;
+	protected _appendage3: string;
+	protected _appendage4: string;
+	protected _appendage5: string;
+	protected _appendage6: string;
+	protected _appendage7: string;
 	constructor(orientation?: IOrientation, height?: IHeight, color?: string)
+	{
+		this.generateGene(orientation, height, color);
+
+	}
+
+	private generateGene(
+		orientation = this.generateRandomOrientation(),
+		height = this.generateRandomHeight(),
+		color = this.generateRandomColor())
+	{
+		this._orientation = orientation;
+		this._height = height;
+		this._color = color;
+		this.generateRandomHead();
+		this.generateRandomTorso();
+		this.generateRandomAppendages();
+	}
+
+	private generateRandomOrientation(): IOrientation
+	{
+		return (Math.random() > 0.5) ? 'Horizontal' : 'Vertical';
+	}
+
+	private generateRandomHeight(): IHeight
+	{
+		return (Math.random() > 0.5) ? 'Short' : 'Tall';
+	}
+
+	private generateRandomColor(): string
+	{
+		return '';
+	}
+
+	private generateRandomHead(): void
+	{
+
+	}
+
+	private generateRandomTorso(): void
+	{
+
+	}
+
+	private generateRandomAppendages(): void
 	{
 
 	}
